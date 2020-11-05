ee17f28f4e9c89e19119cd4e1bf49873067433a9
princeew <ericprince82@gmail.com>
2020-11-05 11:52:51 -0700

    adding README.md

--- R/examples/basic_run.R
+++ R/examples/basic_run.R
@@ -1,41 +1,100 @@
-devtools::load_all()
+#!/usr/bin/env Rscript
+basic_run <- function(args) {
+  # Basic example template to run PFFindR
+
+  # If no args provided, run with the preloaded FA dataset
+  if (all(is.na(args))) {
+    cat("Loading Example Dataset...")
+    t_s <- Sys.time()
+    pf_data <- PF_FanconiAnemia
+    cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+  } else {
+    # otherwise use the args to build a dataset
+  }
+
+
+  # Initialize the population of subnetworks
+  cat("\nInitializing Population...")
+  t_s <- Sys.time()
+  population <- initializePopulation(pf_data, population_size = 10, "true_members")
+  cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+
+  # Calculate the p-value of the population
+  cat("\nCalculating Significance of Population...")
+  t_s <- Sys.time()
+  p_value <- evaluatePopulationSignficance(pf_data, population, 10)
+  cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+
+  # Calculate the gene scores from the top network
+  cat("\nCalculating Gene Scores from Top Network...")
+  t_s <- Sys.time()
+  top_network <- getTopNetwork(population)
+  scores <- calculateGeneScores(top_network, pf_data)
+  cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+
+  # Visualize the gene scores
+  cat("\nVisualizing Gene Scores...")
+  t_s <- Sys.time()
+  viewGeneScores(PF_FanconiAnemia, getTopNetwork(population), scores)
+  cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+
+  cat("Saving and Exiting...")
+  t_s <- Sys.time()
+  ggplot2::ggsave("~/Desktop/genescores.png", width = 25, height = 20, units="in")
+  save.image()
+  cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
+}
+
+main <- function() {
+  check_and_maybe_install_dependencies()
+  args <- load_arguments()
+}
+
+check_and_maybe_install_dependencies <- function() {
+  # Install PFFindR if not already present
+  if (!is.element("PFFindR", installed.packages()[,1])) {
+    devtools::install_github("princeew/PFFindR")
+  }
+
+  # Check for argparser
+  if (!is.element("argparse", installed.packages()[,1])) {
+    install.packages("argparse")
+  }
+
+  # load packages
+  suppressPackageStartupMessages(library("argparse"))
+  library("PFFindR")
+}
+
+load_arguments <- function() {
+  # Command line argument handling
+  parser <- ArgumentParser()
+  parser$add_argument("--use_FA_data", action = "store_true", default = FALSE,
+                      help = "Use the example PF_FanconiAnemia dataset. Overrides --loci_data and --cfn_data [default \"%(default)s\"]")
+  parser$add_argument("--loci_data", type = "character",
+                      help = "Path to tab delimented loci data (i.e., a GMT file).")
+  parser$add_argument("--cfn_data", type = "character",
+                      help = "Path to tab delimted co-function network data.")
+  parser$add_argument("--population_size", type = "integer", default = 10,
+                      help = "The number of subnetworks to consider in a population. [default \"%(default)s\"]")
+  parser$add_argument("--null_trials", type = "integer", default = 50,
+                      help = "The number of random trials to evaluate for the null distribtuion to determine population significance. [default \"%(default)s\"]")
+  parser$add_argument("--save_gene_score_plot", type = "logical", default = TRUE,
+                      help = "Save the results from viewGeneScores. [default \"%(default)s\"]")
+  parser$add_argument("--gene_score_plot_path", type = "character", default = "./PFFinderGeneScores.png",
+                      help = "Path to save the gene score visualizations. [default \"%(default)s\"]")
+  parser$add_argument("--uninstall_after", action = "store_true", default = TRUE,
+                      help = "Uninstall PFFindR after running this example. [default \"%(default)s\"]")
+  args <- parser$parse_args()
+  return(args)
+}
+
+main()
+
+
 # Load required input data
 # pf_data <- PFDataLoader(
 #   "../app/preloaded_data/FA_loci.txt",
 #   "../app/preloaded_data/Day3_STRING.txt")
-cat("Loading Example Dataset...")
-t_s <- Sys.time()
-pf_data <- PF_FanconiAnemia
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
 
-# Initialize the population of subnetworks
-cat("\nInitializing Population...")
-t_s <- Sys.time()
-population <- initializePopulation(pf_data, population_size = 10, "true_members")
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
-
-# Calculate the p-value of the population
-cat("\nCalculating Significance of Population...")
-t_s <- Sys.time()
-p_value <- evaluatePopulationSignficance(pf_data, population, 10)
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
-
-# Calculate the gene scores from the top network
-cat("\nCalculating Gene Scores from Top Network...")
-t_s <- Sys.time()
-top_network <- getTopNetwork(population)
-scores <- calculateGeneScores(top_network, pf_data)
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
-
-# Visualize the gene scores
-cat("\nVisualizing Gene Scores...")
-t_s <- Sys.time()
-viewGeneScores(PF_FanconiAnemia, getTopNetwork(population), scores)
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
-
-cat("Saving and Exiting...")
-t_s <- Sys.time()
-ggplot2::ggsave("~/Desktop/genescores.png", width = 25, height = 20, units="in")
-save.image()
-cat(paste0("\n\tDone. Time Elapsed=", round(as.numeric((Sys.time()-t_s)), 2), " s"))
 
---
+++ README.md
@@ -0,0 +1,83 @@
+# PFFindR: Prix Fixe network Finder in R
+
+## Motivation
+
+## Installation
+
+This package is available for download on [github](https://github.com/princeew/PFFindR). 
+The easiest way to accomplish this is to install the package from the R console:
+
+```
+  library(devtools)
+  install_github("princeew/PFFindR")
+```
+Alternatively, you can clone the repository and manually install the package yourself.
+
+```
+  cd <location/to/store/package>
+  git clone princeew/PFFindR
+  R -e "install.packages("./PFFindR", repos=NULL, source=TRUE)"
+```
+
+**NOTE:** If you are only interested in running the example script to test the software,
+the PFFindR package will be automatically installed, executed, and uninstalled.
+This behavior can be modified using the command line options (see Command Line
+Usage Example below).
+
+## Usage
+
+
+### R Console Usage
+
+
+#### Package Documentation
+
+Rich documentation exists for all major functions in the PFFindR package and can
+be accessed using the native R methodology. For example, the `initializePopulation`
+function documentation can be obtained using either of the following two
+(equivalent) commands within the R console:
+
+```
+  help(initializePopulation)
+  ?initializePopulation
+```
+
+### Command Line Usage Example
+
+#### Advanced Usage Options
+
+```
+  usage: ./R/examples/basic_run.R [-h]
+                       [--use_FA_data] [--loci_data LOCI_DATA]
+                       [--cfn_data CFN_DATA] [--population_size POPULATION_SIZE]
+                       [--null_trials NULL_TRIALS]
+                       [--save_gene_score_plot SAVE_GENE_SCORE_PLOT]
+                       [--gene_score_plot_path GENE_SCORE_PLOT_PATH]
+                       [--uninstall_after]
+
+  optional arguments:
+    -h, --help            show this help message and exit
+    --use_FA_data         Use the example PF_FanconiAnemia dataset. Overrides
+                          --loci_data and --cfn_data [default "False"]
+    --loci_data LOCI_DATA
+                          Path to tab delimented loci data (i.e., a GMT file).
+    --cfn_data CFN_DATA   Path to tab delimted co-function network data.
+    --population_size POPULATION_SIZE
+                          The number of subnetworks to consider in a population.
+                          [default "10"]
+    --null_trials NULL_TRIALS
+                          The number of random trials to evaluate for the null
+                          distribtuion to determine population significance.
+                          [default "50"]
+    --save_gene_score_plot SAVE_GENE_SCORE_PLOT
+                          Save the results from viewGeneScores. [default "True"]
+    --gene_score_plot_path GENE_SCORE_PLOT_PATH
+                          Path to save the gene score visualizations. [default
+                          "./PFFinderGeneScores.png"]
+    --uninstall_after     Uninstall PFFindR after running this example. [default
+                          "True"]
+```
+
+### Preloaded Data
+
+### Troubleshooting
